7bc6ec01cb878771c0f33cefaa9340f5e80cae02
aosothra <12795376+aosothra@users.noreply.github.com>
2022-05-03 19:33:58 +0400

    add blank start page

---
+++ where_to_go/views.py
@@ -0,0 +1,8 @@
+from re import template
+from django.http import HttpResponse
+from django.template import loader
+
+def welcome(request):
+    template = loader.get_template('welcome.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
